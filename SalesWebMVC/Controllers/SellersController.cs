846ecbfd8dd588a0ce14a414bec733fe21c69649
GUSTAVO A S VALLE VALLE <Gustavo Valle>
2021-09-07 21:16:52 -0300

    Update seller and custom service exception

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMVC.Models;
+using Sales.Sevices.Exceptions;
 
 namespace Sales.Controllers
 {
@@ -98,5 +99,32 @@ namespace Sales.Controllers
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
             return View(viewModel);
         }
-    }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _sellerService.Update(seller);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+
+                return NotFound();
+            }
+
+            catch (DbConcurrencyException)
+            {
+
+                return BadRequest();
+            }
+        }
 }
